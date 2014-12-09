12b54b18713e8d1509c14ff16b1bee51d516e818
Christophe Gagnier <admin@cgagnier.ca>
2014-12-09 14:58:32 -0500

    Affichage exercice et calories perdu par semaine
    Sur la page d'accueuil

--- index.php
+++ index.php
@@ -8,13 +8,23 @@
 				<h1 class="page-header">Tableau de bord</h1>
 			</div>
 		</div><!--/.row-->
-		
+
+        <div class="row">
+            <div class="col-lg-12">
+                <h2>Objectifs de la semaine</h2>
+            </div>
+        </div><!--/.row-->
+
+
+        <?php
+            $entrainementsSemaine = get_stat_entrainement_semaine($userInfo['utilisateurID']);
+        ?>
 		<div class="row">
 			<div class="col-xs-6 col-md-3">
 				<div class="panel panel-default">
 					<div class="panel-body easypiechart-panel">
 						<h4>Nombre d'entrainement</h4>
-						<div class="easypiechart" id="easypiechart-blue" data-percent="40" ><span class="percent">2/5</span>
+						<div class="easypiechart" id="easypiechart-blue" data-percent="<?php echo $entrainementsSemaine[0] / 5 * 100; ?>" ><span class="percent"><?php echo $entrainementsSemaine[0]; ?>/5</span>
 						</div>
 					</div>
 				</div>
@@ -23,7 +33,7 @@
 				<div class="panel panel-default">
 					<div class="panel-body easypiechart-panel">
 						<h4>Calories perdues</h4>
-						<div class="easypiechart" id="easypiechart-orange" data-percent="58" ><span class="percent">1462/2500</span>
+						<div class="easypiechart" id="easypiechart-orange" data-percent="<?php echo $entrainementsSemaine[1] / 2500 * 100; ?>" ><span class="percent"><?php echo $entrainementsSemaine[1]; ?>/2500</span>
 						</div>
 					</div>
 				</div>
--- model/user_db.php
+++ model/user_db.php
@@ -8,3 +8,15 @@ function get_user($userID) {
 	$result = $result->fetch();
     return $result;
 }
+
+function get_stat_entrainement_semaine($userID)
+{
+    global $db;
+    $query = 'SELECT count(clientID), sum(calorieBrulee) FROM `resultat`
+                WHERE `date` >= adddate(curdate(), INTERVAL 1-DAYOFWEEK(curdate()) DAY)
+                AND `date` <= adddate(curdate(), INTERVAL 7-DAYOFWEEK(curdate()) DAY)
+                AND `clientID` = '.$userID;
+    $result = $db->query($query);
+    $result = $result->fetch();
+    return $result;
+}
