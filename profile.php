bf19ec26dfe6286d291ebd8b4a3d8f0bd34515e7
samuelbernier1996 <samichon32@hotmail.ca>
2014-12-10 12:15:23 -0500

    Ajouté Lien vers profile.php

---
+++ profile.php
@@ -0,0 +1,12 @@
+<?php
+include('view/header.php');
+include('view/sidemenu.php');
+?>
+
+<div class="row">
+    <div class="col-lg-12">
+        <h1 class="page-header">Page vide</h1>
+    </div>
+</div><!--/.row-->
+
+<?php include('view/footer.php'); ?>
