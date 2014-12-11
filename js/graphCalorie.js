502f8e9cc6109a940bf1271803f0ceef97191f77
sDesmecht <samuel.desmecht@gmail.com>
2014-12-11 12:53:33 -0500

    graph calorie brulee

--- js/graphCalorie.js
+++ js/graphCalorie.js
@@ -2,10 +2,40 @@
 
 window.onload = function(){
 
-    $.getJSON( "json/getcaloriessemaine.php?userID=2")
+    var labelsJours = ["Lundi","Mardi","Mercredi","Jeudi","Vendredi","Samedi","Dimanche"];
+
+    $.getJSON( "json/getcaloriessemaine.php")
         .done(function(data) {
-            var lineChartData = {
-                labels : ["Lundi","Mardi","Mercredi","Jeudi","Vendredi","Samedi","Dimanche"],
+            var lineChartData =
+            {
+                labels : labelsJours,
+                datasets :
+                [
+                    {
+                        label: "Calories",
+                        fillColor : "rgba(48, 164, 255, 0.2)",
+                        strokeColor : "rgba(48, 164, 255, 1)",
+                        pointColor : "rgba(48, 164, 255, 1)",
+                        pointStrokeColor : "#fff",
+                        pointHighlightFill : "#fff",
+                        pointHighlightStroke : "rgba(48, 164, 255, 1)",
+                        data : data
+                    }
+                ]
+            }
+            var graphCalorieIng = document.getElementById("graphCalorieIng").getContext("2d");
+            window.myLine = new Chart(graphCalorieIng).Line(lineChartData, {
+                responsive: true
+            });
+        })
+        .fail(function() {
+            console.log( "error" );
+        });
+
+    $.getJSON( "json/getcaloriesbrulees.php")
+        .done(function(data) {
+            var lineChartData2 = {
+                labels : labelsJours,
                 datasets : [
                     {
                         label: "Calories",
@@ -20,16 +50,12 @@ window.onload = function(){
                 ]
             }
             var graphCaloriePerd = document.getElementById("graphCaloriePerd").getContext("2d");
-            var graphCalorieIng = document.getElementById("graphCalorieIng").getContext("2d");
-            window.myLine = new Chart(graphCaloriePerd).Line(lineChartData, {
+            window.myLine = new Chart(graphCaloriePerd).Line(lineChartData2, {
                 responsive: true
             });
-            window.myLine = new Chart(graphCalorieIng).Line(lineChartData, {
-                responsive: true
-            });
+
         })
         .fail(function() {
             console.log( "error" );
         });
-
 };
---
+++ json/getcaloriesbrulees.php
@@ -0,0 +1,28 @@
+<?php
+include('../model/user_db.php');
+include('../model/database.php');
+
+session_start();
+
+$userID = $_SESSION['userID'];
+
+$caloriesParJour = Array();
+
+global $db;
+$query = '
+                    SELECT sum(calorieBrulee) AS caloriesPerdu,  WEEKDAY(`date`) AS dayOfWeek
+                    FROM `resultat`
+                    WHERE `date` >= adddate(curdate(), INTERVAL 1-DAYOFWEEK(curdate()) DAY)
+                    AND `date` <= adddate(curdate(), INTERVAL 7-DAYOFWEEK(curdate()) DAY)
+                    AND `clientID` = '.$userID.'
+                    GROUP BY date';
+
+$result = $db->query($query);
+
+while($data = $result->fetch())
+{
+    $caloriesParJour[$data['dayOfWeek']] = $data['caloriesPerdu'];
+}
+
+
+echo json_encode($caloriesParJour);
