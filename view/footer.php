f49398687ef6e90a9c30694099b8726170e24ed6
Moustachauve <admin@cgagnier.ca>
2014-12-19 20:21:53 -0500

    Correction problème graphique

--- view/footer.php
+++ view/footer.php
@@ -1,5 +1,7 @@
 	<script src="js/template_js/bootstrap.min.js"></script>
     <script src="js/template_js/chart.min.js"></script>
+    <script src="js/template_js/easypiechart.js"></script>
+    <script src="js/template_js/easypiechart-data.js"></script>
     <script src="js/template_js/bootstrap-datepicker.js"></script>
     <script src="js/template_js/jquery.formatter.min.js"></script>
 
